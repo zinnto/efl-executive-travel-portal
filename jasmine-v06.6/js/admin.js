c6a25edca8911d167cc7b334938b0c3c6eb66cb1
zinnto <zennate@gmail.com>
2026-07-11 13:42:34 +0300

    Create admin.js

---
+++ jasmine-v06.6/js/admin.js
@@ -0,0 +1,25 @@
+console.log("Jasmine Admin Loaded");
+
+
+
+const buttons = document.querySelectorAll("button");
+
+
+
+buttons.forEach(button => {
+
+
+button.addEventListener("click", function(){
+
+
+alert(
+
+"This module is being prepared."
+
+);
+
+
+});
+
+
+});
